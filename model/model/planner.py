100bf8539e8b6ac14647ff16cd95196f9a9745ee
hameleon33-88 <145537056+hameleon33-88@users.noreply.github.com>
2026-02-15 14:28:38 +0300

    Create planner.py

---
+++ model/model/planner.py
@@ -0,0 +1,9 @@
+class Planner:
+    def build_plan(self):
+        return [
+            "scan_environment",
+            "detect_items",
+            "pick_items",
+            "sort_items",
+            "pack_items"
+        ]
